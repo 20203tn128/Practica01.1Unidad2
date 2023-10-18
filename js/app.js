e069364c3a083c552abb1689344cb8878d47f9cb
LizClaudia <20203tn128@utez.edu.mx>
2023-10-17 18:51:07 -0600

    modifcaciones

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('APPJS');
 const url = window.location.href;
 const API = `http://localhost:3000/api`;
-let swLocation='/reportes/sw.js';
+let swLocation='./sw.js';
 if(navigator.serviceWorker){// este objet navigator va a ver si existe el serviceworker en el navegador que se esta usando
     if(url.includes('localhost')|| url.includes('127.0.0.1')){
         swLocation = '/sw.js';
